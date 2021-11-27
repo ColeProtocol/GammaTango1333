44ab1d122d3bc407243fc831ae9f264a6b5aa187
fnuhana3 <fnuhana3@gatech.edu>
2021-11-27 18:06:29 -0500

    Add Search Bar

--- tabs/hubScreens/ReadingListScreen.jsx
+++ tabs/hubScreens/ReadingListScreen.jsx
@@ -1,46 +1,53 @@
-import React from 'react';
-import {
-  View, StyleSheet,
-} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-import Colors from '../../constants/Colors';
-import SquareButton from '../../components/SquareButton';
+import Colors from "../../constants/Colors";
+import SquareButton from "../../components/SquareButton";
+import { Searchbar } from "react-native-paper";
 
 export default function BattScreen() {
   const navigation = useNavigation();
+  const [searchQuery, setSearchQuery] = React.useState("");
+
+  const onChangeSearch = (query) => setSearchQuery(query);
   const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: Colors.white,
-      width: '100%',
-      alignItems: 'center',
-      justifyContent: 'center',
+      width: "100%",
+      //alignItems: "center",
+      //justifyContent: "center",
     },
     buttonView: {
-      width: '100%',
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-evenly',
+      width: "100%",
+      flexDirection: "column",
+      //alignItems: "center",
+      justifyContent: "space-evenly",
       paddingBottom: 15,
     },
   });
   return (
     <View style={styles.container}>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
       <View style={[styles.buttonView, { paddingTop: 10 }]}>
         <SquareButton
           name="book"
           text="Book1"
           buttonSize={90}
           textSize={13}
-          onPress={() => navigation.navigate('Book1')}
+          onPress={() => navigation.navigate("Book1")}
         />
         <SquareButton
           name="book"
           text="Book2"
           buttonSize={90}
           textSize={13}
-          onPress={() => navigation.navigate('Book2')}
+          onPress={() => navigation.navigate("Book2")}
         />
       </View>
     </View>
