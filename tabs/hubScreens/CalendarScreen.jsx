ca67bd0a0bc108344df167b3036bf80f4d3a3781
Stuti Shah <sshah631@gatech.edu>
2022-03-11 11:44:13 -0500

    Update Calendar

--- tabs/hubScreens/CalendarScreen.jsx
+++ tabs/hubScreens/CalendarScreen.jsx
@@ -111,9 +111,9 @@ export default function CalendarScreen() {
     <View style={{ paddingTop: 50, flex: 1 }}>
         <Calendar
           // Initially visible month. Default = Date()
-          current={'2022-01-01'}
+          current={Date()}
           // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-          minDate={'2012-05-10'}
+          minDate={Date()}
           // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
           maxDate={'2022-05-30'}
           // Handler which gets executed on day press. Default = undefined
@@ -123,13 +123,13 @@ export default function CalendarScreen() {
                 selectDate(day);
           }}
           // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-          monthFormat={'yyyy MM'}
+          monthFormat={'MMMM yy'}
           // Handler which gets executed when visible month changes in calendar. Default = undefined
           onMonthChange={month => {
             console.log('month changed', month);
           }}
           // Hide month navigation arrows. Default = false
-          hideArrows={true}
+          hideArrows={false}
           // Do not show days of other months in month page. Default = false
           hideExtraDays={true}
           // If hideArrows=false and hideExtraDays=false do not swich month when tapping on greyed out
