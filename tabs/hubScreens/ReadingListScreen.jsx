3a94d4fcf0d72328c95992e043765a06a703f920
Stuti Shah <sshah631@gatech.edu>
2022-04-04 09:34:51 -0400

    Add book to Book1
    Book 1 button leads to the bluebook for now.
    This can be changed later when database is populated with relevant reading material.

--- tabs/hubScreens/ReadingListScreen.jsx
+++ tabs/hubScreens/ReadingListScreen.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
@@ -6,9 +6,50 @@ import Colors from "../../constants/Colors";
 import SquareButton from "../../components/SquareButton";
 import { Searchbar } from "react-native-paper";
 
+import axios from "axios";
+import getData from "../../assets/Async/getData";
+
+let pdfURL;
+
+const getBluebook = async () => {
+  let { jwt } = await getData();
+  const request = await axios.get("http://3.19.67.76:1337/Bluebooks", {
+    headers: {
+      Authorization: `Bearer ${jwt}`,
+    },
+  });
+  const url = request.data;
+  pdfURL = url[0].URL;
+  //console.log(pdfURL);
+};
+
 export default function BattScreen() {
   const navigation = useNavigation();
   const [searchQuery, setSearchQuery] = React.useState("");
+
+  getBluebook();
+
+    useEffect(() => {
+      const getData = async () => {
+        const val = await AsyncStorage.getItem("@user_data").catch(console.log);
+        const json = JSON.parse(val);
+
+        setUsername(json.user.username);
+        setEmail(json.user.email);
+        setID(json.user.id);
+
+        Firebase.app()
+          .firestore()
+          .collection("users")
+          .doc(email)
+          .get()
+          .then((docSnapshot) => {
+            setPicture(docSnapshot.get("picture"));
+          });
+      };
+
+      getData();
+    });
 
   const onChangeSearch = (query) => setSearchQuery(query);
   const styles = StyleSheet.create({
@@ -27,6 +68,7 @@ export default function BattScreen() {
       paddingBottom: 15,
     },
   });
+
   return (
     <View style={styles.container}>
       <Searchbar
@@ -40,7 +82,7 @@ export default function BattScreen() {
           text="Book1"
           buttonSize={90}
           textSize={13}
-          onPress={() => navigation.navigate("Book1")}
+          onPress={() => navigation.navigate("Book1", { bluebookURL: pdfURL })}
         />
         <SquareButton
           name="book"
